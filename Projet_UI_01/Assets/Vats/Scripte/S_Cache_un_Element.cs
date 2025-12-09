677edad582d913a7387b0a3658c97d1ce040c511
vatea_RUI <vrui@gaming.tech>
2025-12-09 10:59:14 +0100

    comite_09_12_2025_11H00

---
+++ Projet_UI_01/Assets/Vats/Scripte/S_Cache_un_Element.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class S_Cache_un_Element : MonoBehaviour
+{
+    [SerializeField] bool cache_Lobjet;
+    [SerializeField] GameObject RefObjet;
+    public void Cache_Objet_Ref()
+    {
+        if (RefObjet != null)
+        {
+            print("yep");
+            if(RefObjet.activeSelf) RefObjet.SetActive(false);
+            else RefObjet.SetActive(true);
+        } 
+        else Debug.LogError("votre objet n'est pas existant");
+    }
+    void Start()
+    {
+        if (cache_Lobjet && RefObjet != null)
+        {
+            RefObjet.SetActive(false);
+        }
+        else Debug.LogError("votre objet n'est pas existant");
+    }
+    
+
+}
---
+++ Projet_UI_01/Assets/Vats/Scripte/S_Inventaire.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class S_Inventaire : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
